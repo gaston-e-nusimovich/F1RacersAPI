96425d24567019b2b6964355f74e63d5740c89bb
gaston-e-nusimovich <gaston_e_nusimovich@hotmail.com>
2023-04-18 10:10:55 -0300

    Add config builder, handle json file appsettings.json, read connection string from appsettings.json into variable, use connection string variable for DbContext config in injection

--- F1RacersAPI/Program.cs
+++ F1RacersAPI/Program.cs
@@ -2,21 +2,34 @@
 using F1RacersAPI.Data;
 using F1RacersAPI.Services.F1RacersService;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace F1RacersAPI
 {
     public class Program
     {
+
+        private static  IConfiguration? _configuration;
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+            var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+
+           
 
             // Add services to the container.
 
             builder.Services.AddScoped<IF1RacersService, F1RacersService>();
             builder.Services.AddControllers();
             builder.Services.AddDbContext<F1RacersDataContext>(options => 
-                                        options.UseSqlServer("Server=DESKTOP-9225AUG\\SQLEXPRESS;Database=F1RacersDb; Trusted_Connection=True; TrustServerCertificate=True;"));
+                                        options.UseSqlServer(connectionString));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
